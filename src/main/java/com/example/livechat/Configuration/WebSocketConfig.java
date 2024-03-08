e806d289b1fbe88e5ea227e36392c0e44cd85230
cree-per-com <kcossld-naver.com>
2024-03-08 17:29:09 +0900

    made SecurityConfig, WebSocketConfig

---
+++ src/main/java/com/example/livechat/Configuration/WebSocketConfig.java
@@ -0,0 +1,10 @@
+package com.example.livechat.Configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+
+public class WebSocketConfig {
+
+}
