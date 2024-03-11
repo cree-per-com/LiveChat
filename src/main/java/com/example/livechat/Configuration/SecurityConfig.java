90681b7d7b233ba72787a957f9e5232d4e2c4b95
cree-per-com <kcossld-naver.com>
2024-03-11 13:03:53 +0900

    created DB connections

--- src/main/java/com/example/livechat/Configuration/SecurityConfig.java
+++ src/main/java/com/example/livechat/Configuration/SecurityConfig.java
@@ -2,10 +2,25 @@ package com.example.livechat.Configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(au->au.disable());
+        http.formLogin(au->au.disable());
+        http.httpBasic(au->au.disable());
+        http.authorizeHttpRequests((au)->au
+                .requestMatchers("/","/login","/join").permitAll()
+                .anyRequest().authenticated());
+        http.sessionManagement(session->session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
 }
---
+++ src/main/java/com/example/livechat/DAO/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.livechat.DAO;
+
+import com.example.livechat.Entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+}
